6f39af01aa32261372445cc92e62e9a51f6c8d18
msahal <sahal.mohamed.enis@gmail.com>
2018-09-27 15:10:14 +0200

    add rest controller

--- src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.in28minutes.microservices.springcloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class SpringCloudConfigServerApplication {
 
 
@@ -15,4 +17,5 @@ public class SpringCloudConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
+
 }
