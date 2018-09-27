9b96145a124e2923cd64545d4e73e6559ea90f88
msahal <sahal.mohamed.enis@gmail.com>
2018-09-27 15:00:28 +0200

    delete discovery client

--- src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -3,11 +3,9 @@ package com.in28minutes.microservices.springcloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 public class SpringCloudConfigServerApplication {
 
 
